b44a8bd25f3b056da48d7fbeadfca41f8228f250
= <=>
2020-02-01 18:46:25 +0100

    config model and admin file

--- lib/admin.py
+++ lib/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class BooksInstanceInline(admin.TabularInline):
+    model = models.BookInstance
+
+# Decorate
+# admin.site.register(models.Author ,AuthorAdmin )
+@admin.register(models.Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+# admin.site.register(models.Book)
+@admin.register(models.Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    list_filter = ('title', 'author')
+    inlines = [BooksInstanceInline]
+
+
+# admin.site.register(models.Genre)
+@admin.register(models.Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+# admin.site.register(models.BookInstance)
+@admin.register(models.BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+
+
