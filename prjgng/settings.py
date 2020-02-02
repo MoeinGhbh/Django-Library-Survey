8e5d5f2b374c2b34800e1bec297c83ef86ee4845
= <=>
2020-02-02 12:33:14 +0100

    master detail of Survey Project

--- prjgng/settings.py
+++ prjgng/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'lib.apps.LibConfig',
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
